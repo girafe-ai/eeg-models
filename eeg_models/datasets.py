8cd77ac7699d24530e2e352a299b2402c18e938a
kerventkm <kerventkm@gmail.com>
2021-11-19 02:10:27 +0500

    pre commit autoupdate

--- eeg_models/datasets.py
+++ eeg_models/datasets.py
@@ -1,12 +1,14 @@
-from eeg_models.types import Optional, Callable, Dict, Any, Directory, List
-import torch
 import moabb.datasets
+import torch
+
+from eeg_models.types import Any, Callable, Dict, Directory, List, Optional
+
 
 class AbstractEegDataset:
     def __init__(
         self,
         root: Optional[Directory] = None,
-        split: str = 'train',
+        split: str = "train",
         transforms: Optional[Callable] = None,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
@@ -35,8 +37,8 @@ class AbstractEegDataset:
         raise NotImplementedError()
 
 
-class BraininvaedersDataset(AbstractEegDataset):  
-    def __init__(self, m_data):        
+class BraininvaedersDataset(AbstractEegDataset):
+    def __init__(self, m_data):
         m_dataset = moabb.datasets.bi2013a(
             NonAdaptive=True,
             Adaptive=True,
@@ -47,25 +49,25 @@ class BraininvaedersDataset(AbstractEegDataset):
         m_dataset.download()
 
         m_data = m_dataset.get_data()
-        self.m_data = m_data       
-        
+        self.m_data = m_data
+
     def raw_dataset(self):
         raw_dataset = []
         for _, sessions in sorted(self.m_data.items()):
             eegs, markers = [], []
-            for item, run in sorted(sessions['session_1'].items()):
+            for _item, run in sorted(sessions["session_1"].items()):
                 data = run.get_data()
                 eegs.append(data[:-1])
                 markers.append(data[-1])
-        raw_dataset.append((eegs, markers))        
+        raw_dataset.append((eegs, markers))
         return raw_dataset
-    
+
     def channels(self):
-        return self.m_data[1]['session_1']['run_1'].ch_names[:-1]        
+        return self.m_data[1]["session_1"]["run_1"].ch_names[:-1]
 
     def __getitem__(self, item_idx: int):
-        current_sample = self.m_data[item_idx]        
-        return torch.tensor(current_sample)  
+        current_sample = self.m_data[item_idx]
+        return torch.tensor(current_sample)
 
     def __len__(self) -> int:
         return len(self.m_data)
@@ -81,7 +83,7 @@ class Dataset:
         current_target = self.targets[item_idx]
         return {
             "sample": torch.tensor(current_sample),
-            "target": torch.tensor(current_target)
+            "target": torch.tensor(current_target),
         }
 
     def __len__(self) -> int:
