792445222d130ca9aa746b0865c80317dcc91930
kerventkm <kerventkm@gmail.com>
2021-11-20 20:30:34 +0500

    did some mistakes on setup.cfg file

--- eeg_models/datasets.py
+++ eeg_models/datasets.py
@@ -37,8 +37,8 @@ class AbstractEegDataset:
         raise NotImplementedError()
 
 
-class BraininvaedersDataset(AbstractEegDataset):
-    def __init__(self, m_data):
+class BrainInvadersDataset(AbstractEegDataset):
+    def __init__(self):
         m_dataset = moabb.datasets.bi2013a(
             NonAdaptive=True,
             Adaptive=True,
@@ -48,43 +48,26 @@ class BraininvaedersDataset(AbstractEegDataset):
 
         m_dataset.download()
 
-        m_data = m_dataset.get_data()
-        self.m_data = m_data
+        self.data = m_dataset.get_data()
 
     def raw_dataset(self):
         raw_dataset = []
-        for _, sessions in sorted(self.m_data.items()):
+        for _, sessions in sorted(self.data.items()):
             eegs, markers = [], []
             for _item, run in sorted(sessions["session_1"].items()):
                 data = run.get_data()
                 eegs.append(data[:-1])
                 markers.append(data[-1])
-        raw_dataset.append((eegs, markers))
+            raw_dataset.append((eegs, markers))
         return raw_dataset
 
     def channels(self):
-        return self.m_data[1]["session_1"]["run_1"].ch_names[:-1]
-
-    def __getitem__(self, item_idx: int):
-        current_sample = self.m_data[item_idx]
-        return torch.tensor(current_sample)
-
-    def __len__(self) -> int:
-        return len(self.m_data)
-
-
-class Dataset:
-    def __init__(self, data, targets):
-        self.data = data
-        self.targets = targets
+        return self.data[1]["session_1"]["run_1"].ch_names[:-1]
 
     def __getitem__(self, item_idx: int) -> dict:
-        current_sample = self.data[item_idx, :]
-        current_target = self.targets[item_idx]
-        return {
-            "sample": torch.tensor(current_sample),
-            "target": torch.tensor(current_target),
-        }
+        self.current_subject = item_idx
+        self.current_data = self.data[item_idx]
+        return {"subject": self.current_subject, "data": self.current_data}
 
     def __len__(self) -> int:
         return len(self.data)
