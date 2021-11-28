144e4f8cce13473f0f72bb72e6e8b75c341f5498
Kerven Durdymyradov <kerventkm@gmail.com>
2021-11-28 13:34:29 +0500

    Dataset abstract class and braininvaders (#5)
    * uploaded dataset class file
    
    * Delete datasets.py
    
    * added
    
    * added Abstract dataset
    
    * added channels property
    
    * pre-commit autoupdate
    
    * pre commit autoupdate
    
    * pre-commit autoupdate
    
    * added torch
    
    * reseted after trying MOABB
    
    * poetry add moabb
    
    * did some mistakes on setup.cfg file
    
    * updated datasets.py file
    
    * uploaded dataset class file
    
    * Delete datasets.py
    
    * added
    
    * added Abstract dataset
    
    * added channels property
    
    * pre commit autoupdate
    
    * pre-commit autoupdate
    
    * added torch
    
    * reseted after trying MOABB
    
    * poetry add moabb
    
    * did some mistakes on setup.cfg file
    
    * updated datasets.py file
    
    * updated datasets.py file
    
    * synced pre-commit fils
    
    * returned style check
    
    * removed unnecessary import
    
    * added sections to dependencies
    
    * refactored dataset classes a bit
    
    * update datasets.py
    
    * reset
    
    * fixed behaviour
    
    Co-authored-by: Vladislav Goncharenko <vladislav.goncharenko@phystech.edu>

--- eeg_models/types.py
+++ eeg_models/types.py
@@ -1,6 +1,9 @@
-from typing import List, NewType, Optional
+from pathlib import Path
+from typing import Any, Callable, Dict, List, NewType, Optional
 
 import numpy as np
 
 
+Directory = Path
+File = Path
 npArray = NewType("nupmy array", np.ndarray)
