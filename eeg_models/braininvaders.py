007c77fea9dcf9e776d2675d8eb737c884257268
kerventkm <kerventkm@gmail.com>
2021-12-18 13:57:46 +0500

    braininvaders.py updated

--- eeg_models/braininvaders.py
+++ eeg_models/braininvaders.py
@@ -13,24 +13,23 @@ BI2013a_URL = "https://zenodo.org/record/1494240/files/"
 
 
 class bi2013a(BaseDataset):
-    def __init__(self, NonAdaptive=True, Adaptive=False, Training=True, Online=False):
+    def __init__(self, non_adaptive=True, adaptive=False, training=True, online=False):
         super().__init__(
             subjects=list(range(1, 24 + 1)),
             sessions_per_subject=1,
-            events=dict(Target=1, NonTarget=2),
+            events=dict(target=1, non_target=2),
             code="Brain Invaders 2013a",
             interval=[0, 1],
             paradigm="p300",
             doi="",
         )
 
-        self.adaptive = Adaptive
-        self.nonadaptive = NonAdaptive
-        self.training = Training
-        self.online = Online
+        self.adaptive = adaptive
+        self.non_adaptive = non_adaptive
+        self.training = training
+        self.online = online
 
     def _get_single_subject_data(self, subject):
-        """return data for a single subject"""
 
         file_path_list = self.data_path(subject)
         sessions = {}
@@ -61,18 +60,15 @@ class bi2013a(BaseDataset):
         if subject not in self.subject_list:
             raise (ValueError("Invalid subject number"))
 
-        # check if has the .zip
         url = "{:s}subject{:d}.zip".format(BI2013a_URL, subject)
         path_zip = dl.data_dl(url, "BRAININVADERS")
         path_folder = path_zip.strip("subject{:d}.zip".format(subject))
 
-        # check if has to unzip
         if not (os.path.isdir(path_folder + "subject{:d}".format(subject))):
             print("unzip", path_zip)
             zip_ref = zipfile.ZipFile(path_zip, "r")
             zip_ref.extractall(path_folder)
 
-        # filter the data regarding the experimental conditions
         meta_file = os.path.join("subject{:d}".format(subject), "meta.yml")
         meta_path = path_folder + meta_file
         with open(meta_path, "r") as stream:
@@ -80,8 +76,8 @@ class bi2013a(BaseDataset):
         conditions = []
         if self.adaptive:
             conditions = conditions + ["adaptive"]
-        if self.nonadaptive:
-            conditions = conditions + ["nonadaptive"]
+        if self.non_adaptive:
+            conditions = conditions + ["non_adaptive"]
         types = []
         if self.training:
             types = types + ["training"]
@@ -94,12 +90,11 @@ class bi2013a(BaseDataset):
             if (run_condition in conditions) and (run_type in types):
                 filenames = filenames + [run["filename"]]
 
-        # list the filepaths for this subject
         subject_paths = []
         for filename in filenames:
             subject_paths = subject_paths + glob.glob(
                 os.path.join(
                     path_folder, "subject{:d}".format(subject), "Session*", filename
                 )
-            )  # noqa
+            )
         return subject_paths
