b7cefd9410172340c5aed48406ee819505ebb611
kerventkm <kerventkm@gmail.com>
2021-12-19 14:42:43 +0500

    tests

--- tests/test_eegnet_parameters.py
+++ tests/test_eegnet_parameters.py
@@ -1,8 +1,5 @@
 from eeg_models.EegNet import EegNet
 
 
-net = EegNet(n_classes=2)
-
-
 def test_number_of_parameters():
-    assert sum(p.numel() for p in net.parameters()) == 2256
+    assert sum(p.numel() for p in EegNet(n_classes=2).parameters()) == 2256
