bcfc57740bc32a38e62a4b276c503ad89ca517c9
kerventkm <kerventkm@gmail.com>
2022-01-20 08:45:15 +0500

    updated eegnet

--- eeg_models/eegnet.py
+++ eeg_models/eegnet.py
@@ -1,12 +1,15 @@
+import torch
 import torch.nn as nn
+
+from eeg_models.types import Device, Optional
 
 
 class EegNet(nn.Sequential):
     """Pytorch Implementation of EegNet.
 
-    This implementation is mostly based on the original paper "EEGNet: A Compact Convolutional Network
-    for EEG-based Brain-Computer Interfaces" which can be found on the "https://arxiv.org/abs/1611.08024"
-    and repository is: 'https://github.com/vlawhern/arl-eegmodels'.
+    This model implementats the original paper "EEGNet: A Compact Convolutional Network
+    for EEG-based Brain-Computer Interfaces" which can be found on the https://arxiv.org/abs/1611.08024.
+    Authors' original implementation on Tensorflow: https://github.com/vlawhern/arl-eegmodels.
 
     Assumes the input signal is sampled at 128Hz and used 64 channels. If you want to use this model
     for any other sampling rate and channels, you will need to modify the lengths of temporal
@@ -32,51 +35,44 @@ class EegNet(nn.Sequential):
         rate: int = 128,
         f1: int = 8,
         d: int = 2,
-        f2=None,
-        device=None,
+        f2: Optional[int] = None,
+        device: Optional[Device] = None,
         dtype=None,
     ) -> None:
         self.device = device
         self.dtype = dtype
+
         kernel = rate // 2
         if f2 is None:
-            f2 = f1 * d
+            f2 = d * f1
+        rate_drops = (4, 8)
+
         super().__init__(
             nn.Unflatten(0, (1, 1, n_channels)),
-            nn.Conv2d(1, f1, (1, kernel), padding="same", bias=False).to(
-                device=self.device, dtype=self.dtype
-            ),
-            nn.BatchNorm2d(f1, track_running_stats=False).to(
-                device=self.device, dtype=self.dtype
-            ),
-            nn.Conv2d(f1, d * f1, (n_channels, 1), groups=f1, bias=False).to(
-                device=self.device, dtype=self.dtype
-            ),
-            nn.BatchNorm2d(d * f1, track_running_stats=False).to(
-                device=self.device, dtype=self.dtype
-            ),
-            nn.ELU(),
-            nn.AvgPool2d((1, 4)),
+            # block 1
+            nn.Conv2d(1, f1, (1, kernel), padding="same", bias=False),
+            nn.BatchNorm2d(f1),
+            nn.Conv2d(f1, d * f1, (n_channels, 1), groups=f1, bias=False),
+            nn.BatchNorm2d(d * f1),
+            nn.ELU(inplace=True),
+            nn.AvgPool2d((1, rate_drops[0])),
             nn.Dropout(dropout_rate),
+            # block 2
             nn.Conv2d(
                 d * f1,
                 d * f1,
-                (1, kernel // 4),
+                (1, kernel // rate_drops[0]),
                 padding="same",
-                groups=f1 * d,
+                groups=d * f1,
                 bias=False,
-            ).to(device=self.device, dtype=self.dtype),
-            nn.Conv2d(d * f1, d * f1, 1, bias=False).to(
-                device=self.device, dtype=self.dtype
             ),
-            nn.BatchNorm2d(d * f1, track_running_stats=False).to(
-                device=self.device, dtype=self.dtype
-            ),
-            nn.ELU(),
-            nn.AvgPool2d((1, 8)),
+            nn.Conv2d(d * f1, f2, 1, bias=False),
+            nn.BatchNorm2d(f2),
+            nn.ELU(inplace=True),
+            nn.AvgPool2d((1, rate_drops[1])),
             nn.Dropout(dropout_rate),
             nn.Flatten(),
-            nn.Linear(f2 * (n_samples // 32), n_classes, False).to(
-                device=self.device, dtype=self.dtype
-            ),
+            nn.Linear(f2 * (n_samples // torch.prod(rate_drops)), n_classes),
         )
+
+        self.to(self.device, self.dtype)
