93e306125a2a939980a415c64a0dc4c916fe21e4
kerventkm <kerventkm@gmail.com>
2022-01-20 08:51:07 +0500

    types updated

--- eeg_models/types.py
+++ eeg_models/types.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from typing import Any, Callable, Dict, List, NewType, Optional
 
 import numpy as np
+import torch
 
 
 Directory = Path
 File = Path
 npArray = NewType("nupmy array", np.ndarray)
+Device = torch.device
