b029ded0df4f07274f5a1b6015b55b7458a98b37
Vladislav Goncharenko <vladislav.goncharenko@phystech.edu>
2022-05-10 23:33:03 +0300

    added basic reading methods

--- eeg_models/datasets/demons.py
+++ eeg_models/datasets/demons.py
@@ -1,5 +1,17 @@
+import h5py
 import numpy as np
-from somepytools.typing import Callable, Dict, Directory, Optional, Sequence, Tuple
+import pandas as pd
+from somepytools.typing import (
+    Any,
+    Array,
+    Callable,
+    Dict,
+    Directory,
+    File,
+    Optional,
+    Sequence,
+    Tuple,
+)
 
 from .. import data_dir
 from .abstract import AbstractEegDataset
@@ -7,6 +19,8 @@ from .constants import SPLIT_TRAIN
 
 
 class DemonsP300Dataset(AbstractEegDataset):
+    _default_root = "demons_p300_dataset"
+
     # from somepytools.constants import TIME_CONSTANTS
     _ms_in_sec = 1000
     _hdf_path = "p300dataset"
@@ -39,9 +53,16 @@ class DemonsP300Dataset(AbstractEegDataset):
         download: bool = True,
     ):
         if root is None:
-            root = data_dir / "nery_demons_dataset"
-            # self._dvc = True
+            root = data_dir / self._default_root
         super().__init__(root, split, transforms, transform, target_transform, download)
+
+        self.meta = pd.read_csv(self.root / "meta.csv")
+
+    def __len__(self):
+        return len(self.meta)
+
+    def __getitem__(self, index: int) -> Dict[str, Any]:
+        return self._read_hdf(self.root / self.meta.loc[index, "filename"])
 
     @property
     def channels(self) -> Tuple[str]:
@@ -61,3 +82,34 @@ class DemonsP300Dataset(AbstractEegDataset):
 
     def download(self):
         pass  # nothing to do - this is DVC based dataset
+
+    @classmethod
+    def _read_hdf(cls, filename: File) -> Array:
+        """Reads data from HDF file
+
+        Returns:
+            array of `_act_dtype`
+        """
+        with h5py.File(filename.as_posix(), "r") as hfile:
+            group = hfile[cls._hdf_path]
+            record = np.empty(len(group), cls._act_dtype)
+            for i, act in enumerate(group.values()):
+                record[i]["sessions"] = np.array(
+                    [cls._strip(item) for item in act], cls._session_dtype
+                )
+                for name, value in act.attrs.items():
+                    record[i][name] = value
+        return record
+
+    @staticmethod
+    def _strip(session) -> tuple:
+        """Strips nans (from right side of all channels) added during hdf5 packaging
+
+        Returns:
+            tuple ready to be converted to `_session_dtype`
+        """
+        eeg, *rest = session
+        ind = -next(i for i, value in enumerate(eeg[0, ::-1]) if not np.isnan(value))
+        if ind == 0:
+            ind = None
+        return tuple((eeg[:, :ind], *rest))
